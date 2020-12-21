d99f0b825f12daa5a3029eaf8405d2a4b518b078
Youn-Hee Han <yh21.han@gmail.com>
2020-12-21 18:00:11 +0900

    ....

---
+++ algorithms/extended_baseline.py
@@ -0,0 +1,14 @@
+import networkx as nx
+import copy
+
+# Baseline Agent
+from algorithms.baseline import BaselineVNEAgent
+from common import utils
+
+
+class ExtendedBaselineVNEAgent(BaselineVNEAgent):
+    def __init__(self):
+        pass
+
+    def get_action(self, state):
+        pass
