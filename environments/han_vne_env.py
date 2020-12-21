54f6f74b75fd191ae235a69cacb6e99377e8d321
Youn-Hee Han <yh21.han@gmail.com>
2020-12-21 11:18:57 +0900

    han_ven_env 추가

---
+++ environments/han_vne_env.py
@@ -0,0 +1,184 @@
+import gym
+import networkx as nx
+import numpy as np
+from random import randint, expovariate
+
+
+class VNETestEnvironment(gym.Env):
+    def __init__(self, global_max_step):
+        self.GLOBAL_MAX_STEPS = global_max_step
+        self.SUBSTRATE_NET = None
+
+        self.VNRs_ARRIVED = None
+        self.VNRs_INFO = None
+        self.VNRs_SERVED = None
+        self.VNRs_COLLECTED_UNTIL_NEXT_EMBEDDING_EPOCH = None
+
+        self.step_idx = None
+        self.action_step_idx = None
+        self.total_acceptance = None
+        self.accept_vnr_counts = None
+        self.vnr_arrival_time_steps = None
+
+    def reset(self):
+        # Each substrate network is configured to have 100 nodes with over 500 links,
+        # which is about the scale of a medium-sized ISP.
+        self.SUBSTRATE_NET = nx.gnm_random_graph(n=100, m=500)
+
+        # corresponding CPU and bandwidth resources of it are real numbers uniformly distributed from 50 to 100
+        for node_id in self.SUBSTRATE_NET.nodes:
+            self.SUBSTRATE_NET.nodes[node_id]['CPU'] = randint(50, 100)
+
+        for edge_id in self.SUBSTRATE_NET.edges:
+            self.SUBSTRATE_NET.edges[edge_id]['bandwidth'] = randint(50, 100)
+
+        self.VNRs_ARRIVED = np.zeros(self.GLOBAL_MAX_STEPS)
+        self.VNRs_INFO = {}
+        self.VNRs_COLLECTED = []
+        self.VNRs_SERVED = []
+
+        time_step = 0
+        vnr_id = 0
+        while True:
+            # The arrival of VNRs follows a Poisson process with an average arrival rate of 5 VNs per 100 time units.
+            # inter-arrival time mean: 0.05
+            next_arrival = int(expovariate(0.05))
+
+            time_step += next_arrival
+            if time_step > self.GLOBAL_MAX_STEPS:
+                break
+
+            self.VNRs_ARRIVED[time_step] += 1
+
+            new_vnr_net, duration, delay = self._get_new_vnr()
+            vnr = {
+                "id": vnr_id,
+                "start_time_step": time_step,
+                "graph": new_vnr_net,
+                "duration": duration,
+                "delay": delay
+            }
+            if time_step not in self.VNRs_INFO:
+                self.VNRs_INFO[time_step] = []
+
+            self.VNRs_INFO[time_step].append(vnr)
+
+        self.step_idx = 0
+        self.action_step_idx = 0
+        self.total_acceptance = 0
+        self.accept_vnr_counts = 0
+
+        self.collect_vnr_for_time_step(self.step_idx)
+
+        initial_state = {}
+        initial_state["substrate_net"] = self.SUBSTRATE_NET
+        initial_state["vnrs_collected"] = self.VNRs_COLLECTED
+
+        return initial_state
+
+    def collect_vnr_for_time_step(self, time_step):
+        if time_step in self.VNRs_INFO:
+            for vnr in self.VNRs_INFO[time_step]:
+                self.VNRs_COLLECTED.append(vnr)
+
+    @staticmethod
+    def _get_new_vnr():
+        # each VN has an exponentially distributed duration with an average of 500 time units
+        # duration mean: 0.002
+        duration = int(expovariate(0.002))
+
+        # the delay is set to be 200 time units
+        delay = 200
+
+        # The number of nodes in a VNR is configured by a uniform distribution between 5 and 20.
+        num_nodes = randint(5, 20)
+
+        # Pairs of virtual nodes are randomly connected by links with the probability of 0.5.
+        new_vnr_net = nx.gnp_random_graph(n=num_nodes, p=0.5)
+
+        # CPU and bandwidth requirements of virtual nodes and links are real numbers uniformly distributed between 1 and 50.
+        for node_id in new_vnr_net.nodes:
+            new_vnr_net.nodes[node_id]['CPU'] = randint(1, 50)
+        for edge_id in new_vnr_net.edges:
+            new_vnr_net.edges[edge_id]['bandwidth'] = randint(1, 50)
+
+        return new_vnr_net, duration, delay
+
+    def _embed_substrate_network(self, embedded_nodes, embedded_links):
+        # embed 'embedded_nodes' into the substrate nodes
+        for v_node_id in embedded_nodes:
+            for s_node_id, cpu_demand in [embedded_nodes[v_node_id]]:
+                self.SUBSTRATE_NET.nodes[s_node_id]['CPU'] -= cpu_demand
+
+        # embed 'embedded_links' into the substrate links
+        for v_link_id in embedded_links:
+            for s_link_id, v_bandwidth_demand in [embedded_links[v_link_id]]:
+                for node_id in range(len(s_link_id) - 1):
+                    self.SUBSTRATE_NET.edges[s_link_id[node_id], s_link_id[node_id + 1]]['bandwidth'] -= v_bandwidth_demand
+
+    def _check_served_vnr(self):
+        served_total_revenue = 0
+        for served_vnr, embedded_nodes, embedded_links in self.VNRs_SERVED:
+            served_vnr['duration'] -= self.vnr_arrival_time_steps[self.action_step_idx]
+            if served_vnr['duration'] <= 0:
+                for v_node_id in embedded_nodes:
+                    for s_node_id, cpu_demand in [embedded_nodes[v_node_id]]:
+                        self.SUBSTRATE_NET.nodes[s_node_id]['CPU'] += cpu_demand
+                for v_link_id in embedded_links:
+                    for s_link_id, v_bandwidth_demand in [embedded_links[v_link_id]]:
+                        for node_id in range(len(s_link_id) - 1):
+                            self.SUBSTRATE_NET.edges[s_link_id[node_id], s_link_id[node_id + 1]]['bandwidth'] += v_bandwidth_demand
+                self.VNRs_SERVED.remove([served_vnr, embedded_nodes, embedded_links])
+
+        for served_vnr, embedded_nodes, embedded_links in self.VNRs_SERVED:
+            served_total_revenue += self._revenue_VNR(served_vnr['graph'])
+
+        return served_total_revenue
+
+    def _check_waiting_vnr(self):
+        for waiting_vnr in self.VNRs_INFO[self.vnr_arrival_time_steps[self.action_step_idx + 1]]:
+            if waiting_vnr['delay'] <= 0:
+                self.VNRs_INFO[self.vnr_arrival_time_steps[self.action_step_idx + 1]].remove(waiting_vnr)
+
+    @staticmethod
+    def _revenue_VNR(vnr):
+        revenue_cpu = 0.0
+        for node_id in vnr.nodes:
+            revenue_cpu += vnr.nodes[node_id]['CPU']
+
+        revenue_bandwidth = 0.0
+        for edge_id in vnr.edges:
+            revenue_bandwidth += vnr.edges[edge_id]['bandwidth']
+
+        alpha = 0.8
+
+        revenue = revenue_cpu + alpha * revenue_bandwidth
+
+        return revenue
+
+    def step(self, action):
+        self.step_idx += 1
+
+        if action:
+            pass
+            self.VNRs_COLLECTED.clear()
+        else:
+            self.collect_vnr_for_time_step(self.step_idx)
+
+        reward = 0.0
+
+        if self.step_idx >= self.GLOBAL_MAX_STEPS:
+            done = True
+        else:
+            done = False
+
+        next_state = {}
+        next_state["substrate_net"] = self.SUBSTRATE_NET
+        next_state["vnrs_collected"] = self.VNRs_COLLECTED
+
+        info = {
+            "acceptance_ratio": 0.0
+        }
+
+        return next_state, reward, done, info
+
--- main/baseline_main.py
+++ main/baseline_main.py
@@ -1,84 +1,88 @@
-from environments.vne_env import VNETestEnvironment
+from environments.han_vne_env import VNETestEnvironment
 from algorithms.baseline import BaselineVNEAgent
 
 import matplotlib.pyplot as plt
 import os
 import glob
 
-
 PROJECT_HOME = os.getcwd()[:-5]
 graph_save_path = PROJECT_HOME + "/graphs/"
 
-env = VNETestEnvironment()
-bl_agent = BaselineVNEAgent()
 
-state = env.reset()
-next_state = []
-done = False
+TIME_STEP_SCALE = 1 / 10
 
-episode_reward = 0.0
+GLOBAL_MAX_STEP = int(56000 * TIME_STEP_SCALE)
 
-# extract the arrival time
-arrival_idx = []
-for idx in state[1]:
-    arrival_idx.append(idx)
+TIME_WINDOW_SIZE = int(500 * TIME_STEP_SCALE)
 
-step_rewards = []
-time_step = 0
-action_time_step = 0
-time_steps = []
-acceptance_ratios = []
 
-while not done:
-    # processing the state for baseline Agent
-    if time_step == 0 or arrival_idx[action_time_step] > time_step:
-        action = None
+def main():
+    env = VNETestEnvironment(GLOBAL_MAX_STEP)
+    bl_agent = BaselineVNEAgent()
+
+    state = env.reset()
+    next_state = []
+    done = False
+
+    episode_reward = 0.0
+
+    # extract the arrival time
+    arrival_idx = []
+    for idx in state[1]:
+        arrival_idx.append(idx)
+
+    step_rewards = []
+    time_step = 0
+    action_time_step = 0
+    time_steps = []
+    acceptance_ratios = []
+
+    next_embedding_epoch = TIME_WINDOW_SIZE
+
+    while not done:
+        time_step += 1
+
+        if time_step < next_embedding_epoch:
+            action = None
+        else:
+            action = bl_agent.get_action(state)
+            next_embedding_epoch += TIME_WINDOW_SIZE
+
         next_state, reward, done, info = env.step(action)
+
         episode_reward += reward
         state = next_state
 
-    else:
-        agent_state = []
-        agent_state.append(state[0])
-        for vnr in state[1][arrival_idx[action_time_step]]:
-            agent_state.append(vnr['graph'])
+        time_steps.append(time_step)
+        step_rewards.append(reward)
+        acceptance_ratios.append(info['acceptance_ratio'])
 
-        action = bl_agent.get_action(agent_state)
+    # save the revenue and acceptance_ratios graph
+    files = glob.glob(os.path.join(PROJECT_HOME, "graphs", "*"))
+    for f in files:
+        os.remove(f)
 
-        next_state, reward, done, info = env.step(action)
-        episode_reward += reward
-        state = next_state
-        action_time_step += 1
+    fig = plt.figure(figsize=(20, 8))
 
-    time_steps.append(time_step)
-    time_step += 1
-    step_rewards.append(reward)
+    ax_1 = fig.add_subplot(2, 1, 1)
+    ax_1.plot(time_steps, step_rewards)
+    ax_1.set_ylabel("Revenue")
+    ax_1.set_xlabel("Time unit")
+    ax_1.set_title("Baseline Agent Revenue")
+    ax_1.grid(True)
 
-    acceptance_ratios.append(info['acceptance_ratio'])
+    ax_2 = fig.add_subplot(2, 1, 2)
+    ax_2.plot(time_steps, acceptance_ratios)
+    ax_2.set_ylabel("Acceptance Ratio")
+    ax_2.set_xlabel("Time unit")
+    ax_2.set_title("Baseline Agent Acceptance Ratio")
+    ax_2.grid(True)
+
+    fig.tight_layout()
+    fig.savefig(os.path.join(graph_save_path, "results.png"))
+
+    print(episode_reward)
 
 
-# save the revenue and acceptance_ratios graph
-files = glob.glob(os.path.join(PROJECT_HOME, "graphs", "*"))
-for f in files:
-    os.remove(f)
-
-fig = plt.figure(figsize=(20,8))
-
-ax_1 = fig.add_subplot(2, 1, 1)
-ax_1.plot(time_steps, step_rewards)
-ax_1.set_ylabel("Revenue")
-ax_1.set_xlabel("Time unit")
-ax_1.set_title("Baseline Agent Revenue")
-ax_1.grid(True)
-
-ax_2 = fig.add_subplot(2, 1, 2)
-ax_2.plot(time_steps, acceptance_ratios)
-ax_2.set_ylabel("Acceptance Ratio")
-ax_2.set_xlabel("Time unit")
-ax_2.set_title("Baseline Agent Acceptance Ratio")
-ax_2.grid(True)
-
-fig.tight_layout()
-fig.savefig(os.path.join(graph_save_path, "results.png"))
-
-print(episode_reward)
+if __name__ == "__main__":
+    main()
