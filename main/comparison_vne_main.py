bd5f80201fb1b4f8e84eb01c9c8484f7eebf14f0
Youn-Hee Han <yh21.han@gmail.com>
2021-03-09 08:25:11 +0900

    ...

--- main/comparison_vne_main.py
+++ main/comparison_vne_main.py
@@ -1,3 +1,10 @@
+import os, sys
+
+current_path = os.path.dirname(os.path.realpath(__file__))
+PROJECT_HOME = os.path.abspath(os.path.join(current_path, os.pardir))
+if PROJECT_HOME not in sys.path:
+    sys.path.append(PROJECT_HOME)
+
 from main.common_main import *
 
 agents = [
