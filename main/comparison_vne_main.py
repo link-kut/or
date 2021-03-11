a4e25d028ef6643eff861279d496c72603572f89
yhhan <yh21.han@gmail.com>
2021-03-11 09:29:54 +0900

    ...

--- main/comparison_vne_main.py
+++ main/comparison_vne_main.py
@@ -9,7 +9,7 @@ from main.common_main import *
 
 agents = [
     BaselineVNEAgent(logger),
-    # TopologyAwareBaselineVNEAgent(0.3, logger),
+    TopologyAwareBaselineVNEAgent(0.3, logger),
     # # EgoNetworkBasedVNEAgent(0.9, logger),
     # EgoNetworkBasedVNEAgent(0.3, logger),
     # DeterministicVNEAgent(logger),
@@ -19,7 +19,7 @@ agents = [
 
 agent_labels = [
     "BL",
-    # "TA_0.3",
+    "TA_0.3",
     # # "EN_0.9",
     # "EN_0.3",
     # "D-ViNE",
